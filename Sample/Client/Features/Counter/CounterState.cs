3b12c6d676520c41a2ad8f22e301fbd4f37ae13a
Ilyana Smith <ilyana@kentsmiths.com>
2021-07-15 10:42:22 -0400

    counterState file added. App does not currently run

---
+++ Sample/Client/Features/Counter/CounterState.cs
@@ -0,0 +1,10 @@
+﻿using BlazorState;
+
+namespace Sample.Client.Features.Counter
+{
+    public partial class CounterState : State<CounterState>
+    {
+        public int Count { get; private set; }
+        public override void Initialize() => Count = 3;
+    }
+}
